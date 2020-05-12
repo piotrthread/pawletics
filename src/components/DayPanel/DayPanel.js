8e35348f6e74593cc72aba1509a6560336ed79bd
Piotr Niciejewski <piotrthread@gmail.com>
2020-05-12 22:19:26 +0200

    added day panel

--- src/components/DayPanel/DayPanel.js
+++ src/components/DayPanel/DayPanel.js
@@ -6,10 +6,9 @@ import { format } from "date-fns";
 const Wrapper = styled.div`
   padding-top: 65px;
   padding-left: 40px;
-  -webkit-box-shadow: inset 4px 0px 9px -2px rgba(0,0,0,0.2);
--moz-box-shadow: inset 4px 0px 9px -2px rgba(0,0,0,0.2);
-box-shadow: inset 4px 0px 9px -2px rgba(0,0,0,0.2);
-Copy Text
+  -webkit-box-shadow: inset 4px 0px 9px -2px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: inset 4px 0px 9px -2px rgba(0, 0, 0, 0.2);
+  box-shadow: inset 4px 0px 9px -2px rgba(0, 0, 0, 0.2);
 `;
 
 const Today = styled.p`
