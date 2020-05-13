6ef550c49987e020bd5b473ba2de3975e0d2a245
Piotr Niciejewski <piotrthread@gmail.com>
2020-05-14 00:00:57 +0200

    fixed loader color

--- src/components/Logging/Logging.js
+++ src/components/Logging/Logging.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Ellipsis } from "react-awesome-spinners";
+import Ellipsis from "./Ellipsis";
 
 const Wrapper = styled.div`
   height: 100vh;
